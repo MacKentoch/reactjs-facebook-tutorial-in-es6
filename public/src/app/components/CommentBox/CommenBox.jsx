66011efade3d3cb4af199801dd699e57fd89202b
MacKentoch <erwan.datin@gmail.com>
2015-12-11 16:42:52 +0100

    chore : checkpoint = tutorial 20

--- public/src/app/components/CommentBox/CommenBox.jsx
+++ public/src/app/components/CommentBox/CommenBox.jsx
@@ -6,14 +6,14 @@ import CommentForm  from './CommentForm/CommentForm.jsx!';
 
 export default class CommentBox extends React.Component {
 	
- constructor(props) {
+  constructor(props) {
   super(props);
   this.init();      //init() = initialization function : just convention to avoid constructor pollution
- }
+  }
  
- init(){
-   this.state = {data: []}; //ES6 way to getInitialState() = set state in constructor
- }
+  init(){
+    this.state = {data: []}; //ES6 way to getInitialState() = set state in constructor
+  }
 
 
   loadCommentsFromServer() {
@@ -29,9 +29,24 @@ export default class CommentBox extends React.Component {
       } //no more need to bind(this) this since ES6 arrow function 
     });
   }
+  
+  handleCommentSubmit(comment) {
+    $.ajax({
+      url       : this.props.url,
+      dataType  : 'json',
+      type      : 'POST',
+      data      : comment,
+      success   : (data)=>{
+        this.setState({data: data});
+      },
+      error     : (xhr, status, err)=>{
+        console.error(this.props.url, status, err.toString());
+      }
+    });
+  }  
 
 
-  componentDidMount() {
+ componentDidMount() {
     this.loadCommentsFromServer();
     //setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval); //need to bind this to comboBox since loadCommentsFromServer() won't execcute in CommentBox context!
     setInterval(()=>this.loadCommentsFromServer, this.props.pollInterval);          //ES6 version to upper line :  save pain with ES6 arrow function :  "()=>this.loadCommentsFromServer === this.loadCommentsFromServer.bind(this)" but don't have to worry about bind(this)
@@ -43,7 +58,7 @@ export default class CommentBox extends React.Component {
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={comment => this.handleCommentSubmit(comment)} />
       </div>
     );
  }
--- public/src/app/components/CommentBox/CommentForm/CommentForm.jsx
+++ public/src/app/components/CommentBox/CommentForm/CommentForm.jsx
@@ -18,22 +18,37 @@ export default class CommentForm extends React.Component {
 	handleTextChange(e) {
 		this.setState({text: e.target.value});
 	}
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		var author 	= this.state.author.trim();
+		var text 		= this.state.text.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.setState({author: '', text: ''});
+	}	
 
  render(){
+	//NOTE on callbacks : you need to bind "this" to refer component instance (React.createClass did the trick for you when ES5) 
+	// "(e)=>this.handleSubmit(e)" is the same as :
+	// "this.handleSubmit.bind(this)"
+	// or "e=>this.handleSubmit(e)"
 	return (
-		<form className="commentForm">
+		<form className="commentForm" onSubmit={(e) => this.handleSubmit(e)}> 
 			<input
 				type="text"
 				placeholder="Your name"
 				value={this.state.author}
-				onChange={(e)=>this.handleAuthorChange(e)} //NOTE : "(e)=>this.handleAuthorChange(e)" is the same as "this.handleAuthorChange.bind(this)"
-			/>
+				onChange={ (e) => this.handleAuthorChange(e) } 
+			/>	
 			<input
 				type="text"
 				placeholder="Say something..."
 				value={this.state.text}
-				onChange={(e)=>this.handleTextChange(e)} //NOTE : "(e)=>this.handleAuthorChange(e)" is the same as "this.handleAuthorChange.bind(this)"
-			/>
+				onChange={(e) => this.handleTextChange(e)} 
+			/> 
 			<input type="submit" value="Post" />
 		</form>
 	);	
