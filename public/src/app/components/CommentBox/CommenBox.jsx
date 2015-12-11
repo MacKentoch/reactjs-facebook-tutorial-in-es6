f3b949ec8d02312847b0715cb5d9b1d79a91fbc8
MacKentoch <erwan.datin@gmail.com>
2015-12-11 07:13:27 +0100

    chore : checkpoint = tutorial 14

--- public/src/app/components/CommentBox/CommenBox.jsx
+++ public/src/app/components/CommentBox/CommenBox.jsx
@@ -1,4 +1,5 @@
 import React        from 'react';
+import $            from 'jquery';
 import CommentList  from './CommentList/CommentList.jsx!';
 import CommentForm  from './CommentForm/CommentForm.jsx!';
  
@@ -7,13 +8,41 @@ export default class CommentBox extends React.Component {
 	
  constructor(props) {
   super(props);
+  this.init();
  }
+ 
+ init(){
+   this.state = {data: []}; //ES6 way to getInitialState() = set state in constructor
+ }
+
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url       : this.props.url,
+      dataType  : 'json',
+      cache     : false,
+      success   : (data)=>{
+        this.setState({data: data});
+      }, //no more need to bind this since ES6 arrow function
+      error     : (xhr, status, err)=>{
+        console.error(this.props.url, status, err.toString());
+      } //no more need to bind this since ES6 arrow function 
+    });
+  }
+
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    //setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval); //need to bind this to comboBox since loadCommentsFromServer() won't execcute in CommentBox context!
+    setInterval(()=>this.loadCommentsFromServer, this.props.pollInterval);    //ES6 version to upper line :  save pain with ES6 arrow function :  "()=>this.loadCommentsFromServer === this.loadCommentsFromServer.bind(this)" but don't have to worry about bind(this)
+  }
+
 
  render(){
 	 return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
--- public/src/app/main.jsx
+++ public/src/app/main.jsx
@@ -1,9 +1,9 @@
 import React        from 'react';
 import ReactDOM     from 'react-dom';
 import CommentBox   from './components/CommentBox/CommenBox.jsx!';     
-import data         from './mockup/data.json!json'; 
+import data         from './mockup/data.json!json';  //mockup data (tutorial step 10)
  
 ReactDOM.render(
-  <CommentBox url="/api/comments" />,
+  <CommentBox url="/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );
